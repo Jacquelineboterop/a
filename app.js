4aaa12c1df3001b70d9a4d5d83eb50f38eac88f0
Jacqueline <proyectos@branta.com.co>
2021-06-10 19:14:44 -0500

    json

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Note = require("./models/Note");
-const path = require('path');
-const md = require('marked');
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/notes', { useNewUrlParser: true });
 
-
-app.set('view engine', 'pug');
-app.set('views', 'views');
-
 app.use(express.urlencoded({ extended: true }));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
 
 app.get("/products", async (req, res) => {
  const notes = await Note.find();
